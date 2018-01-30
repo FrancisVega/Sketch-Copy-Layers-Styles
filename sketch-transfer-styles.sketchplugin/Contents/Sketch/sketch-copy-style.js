02a18ef3053d29068fb7096a35fa85addd706cb6
Francis <hisconer@gmail.com>
2018-01-30 19:50:25 +0100

    Minor refactor

--- sketch-transfer-styles.sketchplugin/Contents/Sketch/sketch-copy-style.js
+++ sketch-transfer-styles.sketchplugin/Contents/Sketch/sketch-copy-style.js
@@ -1,43 +1,38 @@
 // Functions
-const copyInstanceSharedTextStyle = (context, instance) => context.document.documentData().layerTextStyles().sharedStyleForInstance( instance.style() );
-const copyInstanceSharedLayerStyle = (context, instance) => context.document.documentData().layerStyles().sharedStyleForInstance( instance.style() );
+const copyInstanceSharedTextStyle = ( context, instance ) => context.document.documentData().layerTextStyles().sharedStyleForInstance( instance.style() );
+const copyInstanceSharedLayerStyle = ( context, instance ) => context.document.documentData().layerStyles().sharedStyleForInstance( instance.style() );
 const pasteInstanceSharedStyle = ( layer, sharedStyle ) => layer.style = sharedStyle.newInstance();
+const copyInstanceSharedStyle = ( context, instance, klass ) => {
+  if ( klass == 'MSTextLayer' )
+    return copyInstanceSharedTextStyle( context, instance );
+  if ( klass == 'MSShapeGroup' )
+    return copyInstanceSharedLayerStyle( context, instance );
+  return null;
+}
 
-// Main Function
 const transferStyle = ( context, original, targets ) => {
   // Filter target layers by original class
   const originalClass = original.class();
-  const targetsFiltered = targets.slice().filter(layer => layer.class() == originalClass)
+  const targetsFiltered = targets.slice().filter( layer => layer.class() == originalClass );
 
   // Processing targets layers
   targetsFiltered.map( layer => {
-    // Get layers from original
-    let copyStyle;
-    if (original.class() == 'MSTextLayer') {
-      copyStyle = copyInstanceSharedTextStyle( context, original );
-    }
-    if (original.class() == 'MSShapeGroup') {
-      copyStyle = copyInstanceSharedLayerStyle( context, original );
-    }
+    const klass = original.class();
+    copyStyle = copyInstanceSharedStyle( context, original, klass );
     pasteInstanceSharedStyle ( layer, copyStyle );
   });
 }
 
-function transfer (context) {
-  // Consts
-  const SELECTION = context.selection;
-
+function transfer ( context ) {
   // Layers
+  const SELECTION = context.selection;
   const original = SELECTION[0];
   const targets = SELECTION.slice(1, SELECTION.length);
 
-  if (SELECTION.length > 1){
-    // Transfer Style!
+  if ( SELECTION.length > 1 ){
     transferStyle( context, original, targets );
-    // MSG
-    context.document.showMessage("💅🏻 Styles copied!");
+    context.document.showMessage( '💅🏻 Styles copied!' );
   } else {
-    // MSG
-    context.document.showMessage("💩 Select one original layer and target layers");
+    context.document.showMessage( '💩 Select one original layer and target layers' );
   }
 }
