e2dbaaffc1e03e126b0d1c7a0b559231c35d27c6
Francis <hisconer@gmail.com>
2018-01-30 19:40:34 +0100

    Remove functions and conditionals for filtering

--- sketch-transfer-styles.sketchplugin/Contents/Sketch/sketch-copy-style.js
+++ sketch-transfer-styles.sketchplugin/Contents/Sketch/sketch-copy-style.js
@@ -1,17 +1,16 @@
 // Functions
-const allLayersAreEqual = layers => {
-  const firstClass = layers[0].class();
-  const filteredLayers = layers.slice().filter( layer => layer.class() != firstClass );
-  return !filteredLayers.length;
-}
 const copyInstanceSharedTextStyle = (context, instance) => context.document.documentData().layerTextStyles().sharedStyleForInstance( instance.style() );
 const copyInstanceSharedLayerStyle = (context, instance) => context.document.documentData().layerStyles().sharedStyleForInstance( instance.style() );
 const pasteInstanceSharedStyle = ( layer, sharedStyle ) => layer.style = sharedStyle.newInstance();
 
 // Main Function
 const transferStyle = ( context, original, targets ) => {
+  // Filter target layers by original class
+  const originalClass = original.class();
+  const targetsFiltered = targets.slice().filter(layer => layer.class() == originalClass)
+
   // Processing targets layers
-  targets.slice().map( layer => {
+  targetsFiltered.map( layer => {
     // Get layers from original
     let copyStyle;
     if (original.class() == 'MSTextLayer') {
@@ -33,14 +32,12 @@ function transfer (context) {
   const targets = SELECTION.slice(1, SELECTION.length);
 
   if (SELECTION.length > 1){
-    if (allLayersAreEqual(SELECTION)) {
-      // Transfer Style!
-      transferStyle( context, original, targets );
-      context.document.showMessage("💅🏻 Styles copied!");
-    } else {
-    context.document.showMessage("💩 Mixing layers is not ALLOWED! :P");
-    }
+    // Transfer Style!
+    transferStyle( context, original, targets );
+    // MSG
+    context.document.showMessage("💅🏻 Styles copied!");
   } else {
+    // MSG
     context.document.showMessage("💩 Select one original layer and target layers");
   }
 }
